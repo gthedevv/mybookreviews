1c60b17dfebf7c9660505013929e303e42de2699
Gerard Fajardo <gedfaj@gmail.com>
2018-11-16 06:35:04 -0800

    Import BrowserRouter as Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
@@ -8,9 +8,9 @@ import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
