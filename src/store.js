5505330b5a31084e8436f23066a84fb6781b3d49
Gerard Fajardo <gedfaj@gmail.com>
2018-11-16 06:27:04 -0800

    Setup registration and authentication for client.

--- src/store.js
+++ src/store.js
@@ -1,13 +1,33 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
-import { reducer } from 'redux-form';
+import { loadAuthToken } from './local-storage';
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected-data';
+import { setAuthToken, refreshAuthToken } from './actions/auth';
+
+import books from './reducers/books-reducer';
+import users from './reducers/users-reducer';
 
 /* eslint-disable no-underscore-dangle */
-export default createStore(
+const store = createStore(
   combineReducers({
-
-  }), 
+    form: formReducer,
+    auth: authReducer,
+    protectedData: protectedDataReducer,
+    books,
+    users
+  }),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 );
 /* eslint-enable */
+
+const authToken = loadAuthToken();
+if (authToken) {
+  const token = authToken;
+  store.dispatch(setAuthToken(token));
+  store.dispatch(refreshAuthToken());
+}
+
+export default store;
