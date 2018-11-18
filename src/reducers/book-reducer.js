acf9e91c5b8f99f00b5f087b2af3cc938519b3c3
Gerard Fajardo <gedfaj@gmail.com>
2018-11-18 12:29:34 -0800

    Create book-reducer.

---
+++ src/reducers/book-reducer.js
@@ -0,0 +1,27 @@
+import { BOOK_REQUEST, BOOK_SUCCESS, BOOK_ERROR } from "../actions/book";
+
+const initialState = {
+  data: null,
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === BOOK_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true,
+      error: null
+    });
+  } else if (action.type === BOOK_SUCCESS) {
+    return Object.assign({}, state, {
+      loading: false,
+      data: action.payload
+    });
+  } else if (action.type === BOOK_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    });
+  }
+  return state;
+}
