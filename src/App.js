d1c5575b209f96b74d5c77df93579b15f9c3ef33
Gerard Fajardo <gedfaj@gmail.com>
2018-11-18 12:21:17 -0800

    Add Book View route.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './componenets/Home'
+import BookView from './containers/BookView'
 import Layout from './hoc/layout'
 
 
@@ -10,6 +11,8 @@ function App() {
     <Layout>
       <Switch>
         <Route path="/" exact component={Home}/>
+        <Route path="/books/:id" exact component={BookView}/>
+
       </Switch>
     </Layout>
   );
