3b828163b468ad45e4aab8b70825212ae8d684fb
Gerard Fajardo <gedfaj@gmail.com>
2018-11-17 17:53:55 -0800

    Update books reducer to handle all books actions.

--- src/reducers/books-reducer.js
+++ src/reducers/books-reducer.js
@@ -1,6 +1,31 @@
-export default function (state = {}, action) {
-  switch (action.type) {
-    default:
-      return state;
+import {
+  BOOKS_REQUEST,
+  BOOKS_SUCCESS,
+  BOOKS_ERROR
+} from '../actions/books';
+
+const initialState = {
+  list: null,
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === BOOKS_REQUEST) {
+      return Object.assign({}, state, {
+          loading: true,
+          error: null
+      });
+  } else if (action.type === BOOKS_SUCCESS) {
+      return Object.assign({}, state, {
+          loading: false,
+          list:action.payload
+      });
+  } else if (action.type === BOOKS_ERROR) {
+      return Object.assign({}, state, {
+          loading: false,
+          error: action.error
+      });
   }
+  return state;
 }
