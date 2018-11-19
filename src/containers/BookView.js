55f9c78c185d2f9ae867287508e4d9911b8fdfff
Gerard Fajardo <gedfaj@gmail.com>
2018-11-18 20:14:50 -0800

    Update BookView component to render book data.

--- src/containers/BookView.js
+++ src/containers/BookView.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import MDSpinner from 'react-md-spinner'
 
 import  {getBookWithReviewer } from '../actions/book'
+import BookReview from '../views/BookReview'
 
 class BookView extends Component {
 
@@ -9,18 +11,35 @@ class BookView extends Component {
     this.props.dispatch(getBookWithReviewer(this.props.match.params.id))
   }
 
+  renderBook = () => {
+    const { book } = this.props
+
+    if (book.loading) {
+      return <MDSpinner className="spinner" size="28" />;
+    } 
+    
+    if (book.error) {
+      return <strong>{this.props.error}</strong>;
+    }
+
+    if (book.data !== null && book.data.reviewer !== null ) {
+      return (
+      <BookReview  book={book.data}  />
+    );   
+  }
+}
+
   render() {
-    console.log(this.props)
     return (
       <div>
-        
+        {this.renderBook()}
       </div>
     )
   } 
 }
 
 const mapStatetoProps = (state) => ({
-  book: state.book.data
+  book: state.book
 })
 
 export default connect(mapStatetoProps)(BookView)
