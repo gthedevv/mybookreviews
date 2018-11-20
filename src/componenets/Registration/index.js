4b14507196db6503d6e8e4e16e2a016cd89ba603
Gerard Fajardo <gedfaj@gmail.com>
2018-11-20 06:23:59 -0800

    Remove app title from h2 to avoid redundancy.

--- src/componenets/Registration/index.js
+++ src/componenets/Registration/index.js
@@ -12,7 +12,7 @@ export function RegistrationPage(props) {
     }
     return (
         <div className="home">
-            <h2>Register for My Book Review</h2>
+            <h2>Register</h2>
             <RegistrationForm />
             <Link to="/login">Login</Link>
         </div>
