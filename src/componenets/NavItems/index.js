3a1a7fc9b7f17f7c507c400e20ee3aba4a8bc8a1
Gerard Fajardo <gedfaj@gmail.com>
2018-11-20 23:38:48 -0800

    Update sidebar.
    Sidebar will now only show specific buttons depending if the user is logged in or not.

--- src/componenets/NavItems/index.js
+++ src/componenets/NavItems/index.js
@@ -1,79 +1,105 @@
 import React from 'react'
+import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import {clearAuth} from '../../actions/auth';
+import {clearAuthToken} from '../../local-storage';
 
-export default function NavItems() {
+class NavItems extends React.Component {
 
-  const items = [
-    {
-      type: 'navItem',
-      icon: 'home',
-      text: 'Home',
-      link: '/',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'file-text-o',
-      text: 'Profile',
-      link: '/user',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'file-text-o',
-      text: 'Register',
-      link: '/register',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'sign-in',
-      text: 'Login',
-      link: '/login',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'file-text-o',
-      text: 'My Reviews',
-      link: '/user/user-reviews',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'file-text-o',
-      text: 'Add Reviews',
-      link: '/user/add-review',
-      restricted: false
-    },
-    {
-      type: 'navItem',
-      icon: 'sign-out',
-      text: 'Logout',
-      link: '/logout',
-      restricted: false
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
+  render() {
+    let items = [
+      {
+        type: 'navItem',
+        icon: 'file-text-o',
+        text: 'Register',
+        link: '/register',
+        restricted: false
+      },
+      {
+        type: 'navItem',
+        icon: 'sign-in',
+        text: 'Login',
+        link: '/login',
+        restricted: false
+      }
+    ]
+
+    const itemsLoggedIn = [
+      {
+        type: 'navItem',
+        icon: 'file-text-o',
+        text: 'Profile',
+        link: '/user',
+        restricted: false
+      },
+      {
+        type: 'navItem',
+        icon: 'file-text-o',
+        text: 'My Reviews',
+        link: '/user/user-reviews',
+        restricted: false
+      },
+      {
+        type: 'navItem',
+        icon: 'file-text-o',
+        text: 'Add Reviews',
+        link: '/user/add-review',
+        restricted: false
+      }
+    ]
+
+    const element = (item, i) => (
+      <div key={i} className={item.type}>
+        <Link to={item.link}>
+          <FontAwesome name={item.icon}/>
+          {item.text}
+        </Link>
+      </div>
+    )
+
+    let logOutButton
+
+    if (this.props.loggedIn) {
+      items = itemsLoggedIn
+      logOutButton = (
+        <div className='navItem'>
+        <Link to='/' onClick={() => this.logOut()}>
+          <FontAwesome name='sign-out'/>
+          Logout
+        </Link>
+      </div>
+    );
     }
-  ]
 
-  const element = (item, i) => (
-    <div key={i} className={item.type}>
-      <Link to={item.link}>
-        <FontAwesome name={item.icon}/>
-        {item.text}
-      </Link>
-    </div>
-  )
+    const showItems = () => (
+      items.map((item, i) => {
+        return element(item, i)
+      })
+    )
 
-  const showItems = () => (
-    items.map((item, i) => {
-      return element(item, i)
-    })
-  )
-
-  return (
-    <div>
-      {showItems()}
-    </div>
-  )
+    return (
+      <div>
+        <div className='navItem'>
+        <Link to='/'>
+          <FontAwesome name='home'/>
+          Home
+        </Link>
+      </div>
+        {showItems()}
+        {logOutButton}
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(NavItems);
