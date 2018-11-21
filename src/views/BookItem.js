2774fcf20aa1ec19c6353c68a4b96798ba3f57e3
Gerard Fajardo <gedfaj@gmail.com>
2018-11-20 23:39:43 -0800

    Remove console.log.

--- src/views/BookItem.js
+++ src/views/BookItem.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function BookItem(props) {
-  console.log(props)
   return (
     <Link to={`/books/${props._id}`} className="book_item">
       <div className="book_header">
