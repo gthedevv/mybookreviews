f605b89c028d287da7efa8e4230843226212bfc4
Gerard Fajardo <gedfaj@gmail.com>
2018-12-03 23:08:25 -0800

    Add welcome message and app instructions for new users.

--- src/componenets/Home/index.js
+++ src/componenets/Home/index.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import HomeContainer from '../../containers/Home'
 
-export default function Home() {
-  return (
-    <div>
-      <HomeContainer/>
-    </div>
-  )
+ class Home extends Component {
+
+  renderAppInfo() {
+    if(!(this.props.loggedIn)) {
+      return (
+        <div>
+          <section>
+            <h2>Welcome to My Book Reviews</h2>
+            <p>The one simple and easy to use app that lets you share your book reviews with the world!</p>
+          </section>
+          <section>
+            <h2>How To Use</h2>
+            <p>Select the menu button on the top left and register for an account. Once your account is created you can start writing your own reveiws!</p>
+          </section>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderAppInfo()}
+        <HomeContainer/>
+      </div>
+    )
+  }
 }
 
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+})
+
+export default connect(mapStateToProps)(Home);
