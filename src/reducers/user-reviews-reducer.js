c378aa46964a448ef93287897e4204ef8bc11a6c
Gerard Fajardo <gedfaj@gmail.com>
2018-12-03 18:57:44 -0800

    Create user reviews reducer.

---
+++ src/reducers/user-reviews-reducer.js
@@ -0,0 +1,27 @@
+import { USER_REVIEWS_REQUEST, USER_REVIEWS_SUCCESS, USER_REVIEWS_ERROR } from '../actions/user-reviews';
+
+const initialState = {
+  list: null,
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === USER_REVIEWS_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true,
+      error: null
+    });
+  } else if (action.type === USER_REVIEWS_SUCCESS) {
+    return Object.assign({}, state, {
+      loading: false,
+      list: action.payload
+    });
+  } else if (action.type === USER_REVIEWS_ERROR) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    });
+  }
+  return state;
+}
