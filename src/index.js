cf6d3cb0fbc66772b103f800cb944ecbe0a30995
Gerard Fajardo <gedfaj@gmail.com>
2018-12-10 21:55:35 -0800

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import store from './store';
+require('dotenv').config()
 
 ReactDOM.render(
   <Provider store={store}>
